1a4bca9ac81420ef1c9ec10fd11a63317770b87b
Jay Montoya <jayjoplin9898@.com>
2019-11-25 22:42:24 -0800

    fx

--- FindPath.c
+++ FindPath.c
@@ -74,7 +74,7 @@ int main(int argc, char** argv) {
       if (back(pathList) == NIL) {
         fprintf(outFile, "The distance from %d to %d is infinity\n", u, v);
         fprintf(outFile, "No %d-%d path exists\n\n", u,v);
-      } else {
+      } else if (length(pathList) - 1 >= 0) {
         fprintf(outFile, "The distance from %d to %d is %d\n", u, v, length(pathList) - 1);
         fprintf(outFile, "A shortest %d-%d path is: ", u,v);
         printList(outFile, pathList);
